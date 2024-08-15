8651e748689b228eb337ebf1e9039bdc788b096f
Saiful Islam <inasim4@gmail.com>
2024-08-15 21:17:35 +0600

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "NSL",
+  description: "Everything at One Place",
+  icons: {
+    icon: "/favicon/favicon.ico",
+    shortcut: "/favicon/android-192.png",
+    apple: "/favicon/apple-180.png",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,54 @@
+import Image from "next/image";
+import { FaFacebook, FaYoutube, FaGithub, FaTwitch } from "react-icons/fa";
+
+export default function Home() {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <main
+      className="flex min-h-screen flex-col items-center justify-center p-20"
+      style={{
+        backgroundImage: "url('/bg.png')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}>
+      <div className="flex flex-col justify-center items-center gap-5 lg:gap-10">
+        <Image
+          src="/nsl.png"
+          alt="NSL Logo"
+          className="dark:invert"
+          width={100}
+          height={100}
+          priority
+        />
+        <h1 className="text-2xl lg:text-5xl uppercase text-center font-bold text-gray-900 dark:text-gray-100">
+          NSL is under development
+        </h1>
+        <h2 className="text-md lg:text-xl font-semibold text-center text-gray-900 dark:text-gray-100">
+          Stay connected with us on our social media and avail our web
+          development services and get updates.
+        </h2>
+        <div className="flex justify-center items-center space-x-4 gap-5">
+          <a
+            href="https://www.facebook.com/inasim4Gaming"
+            className="text-gray-900 dark:text-gray-100 hover:text-gray-700 dark:hover:text-gray-300">
+            <FaFacebook size={40} />
+          </a>
+          <a
+            href="https://www.youtube.com/@iNasim4Official"
+            className="text-gray-900 dark:text-gray-100 hover:text-gray-700 dark:hover:text-gray-300">
+            <FaYoutube size={40} />
+          </a>
+          <a
+            href="https://www.twitch.tv/inasim4"
+            className="text-gray-900 dark:text-gray-100 hover:text-gray-700 dark:hover:text-gray-300">
+            <FaTwitch size={40} />
+          </a>
+        </div>
+      </div>
+      <footer className="text-center text-gray-600 dark:text-gray-400 text-sm mt-8">
+        &copy; {currentYear} NSL. All rights reserved.
+      </footer>
+    </main>
+  );
+}
